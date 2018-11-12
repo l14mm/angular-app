9e4c958357fd2ba69a6a64fb649cd67c516f95da
liammoroney <liam.moroney14@gmail.com>
2018-11-12 16:29:47 +0000

    Added a save button to the hero detail component which will update the selected hero. The hero service will then calll a PUT to update the hero and log a message.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,6 +7,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -57,5 +61,13 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
