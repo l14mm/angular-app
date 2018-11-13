90d910adf509504ec8804526476e267ac71e63a7
liammoroney <liam.moroney14@gmail.com>
2018-11-13 14:41:10 +0000

    Added a box on the hero component where you enter a new hero name, when you click the add button the hero service will post the new hero which will create it.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,6 +15,15 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
+  
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
